d9c3904934582a0d981de5ae6d7bfcc9ac21f114
aarush <aarushtpatil2010@gmail.com>
2026-09-15 23:04:49 +0400

    Changed a print statement

--- database.py
+++ database.py
@@ -95,6 +95,7 @@ CREATE TABLE solar_flares(
         proceed = True
 
     else:
-        print('NASA\'s NeoWs/DONKI API returned a 503 Error, i.e., either of their servers are currently down. Please try again after a few hours')
+        print('NASA\'s NeoWs/DONKI API returned a 503 Error, i.e., either of their servers are currently down or an unexpected error has occurred. Please try again.')
         proceed = False
-        
+
+# init_db('2026-08-01', '2026-08-03', '2026-06-20', '2026-07-10')
