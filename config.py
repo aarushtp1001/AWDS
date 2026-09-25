24801d5230e1a7aea9bc6895bccab618005e7309
aarush <aarushtpatil2010@gmail.com>
2026-09-25 19:11:22 +0400

    added quick samples

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ DONKI_BASE_URL = "https://api.nasa.gov/DONKI/FLR"
 
 DB_NAME = "nasa_data.sqlite"
 
-## SAMPLE FOR DONKI - https://api.nasa.gov/DONKI/FLR?startDate=2026-07-20&endDate=2026-08-02&api_key=DEMO_KEY
+## JSON SAMPLE FOR DONKI - https://api.nasa.gov/DONKI/FLR?startDate=2026-07-20&endDate=2026-08-02&api_key=DEMO_KEY
+## JSON SAMPLE FOR NeoWs - https://api.nasa.gov/neo/rest/v1/feed?start_date=2026-08-01&end_date=2026-08-03&api_key=DEMO_KEY
